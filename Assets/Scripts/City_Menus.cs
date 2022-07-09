ac02f92992c705dfb8f20f7300fb7c49b06b3f8e
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-07-09 16:02:26 +0100

    +

--- Assets/Scripts/City_Menus.cs
+++ Assets/Scripts/City_Menus.cs
@@ -1,40 +1,103 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class City_Menus: MonoBehaviour {
 
+    //Resources
+    private int stone;
+    private int gold;
+    private int silver;
+
+    // Menus ------------------------------
     public GameObject board;
     public GameObject navigation;
     public GameObject navigation_optional;
     private Vector3 startPos;
     private Vector3 endPos;
-    private float distance=215f;
-    private float duration=1.1f;
-    private float currentTime=0;
-    private bool isPressed=false;
+    private Vector3 startPos2;
+    private Vector3 endPos2;
+    private float distance;
+    private float duration;
+    private float currentTime;
+    private bool isPressed;
+    private bool goUp;
+    private bool validation;
 
     void Start(){
+        //Resources
+        stone = 80;
+        gold = 40;
+        silver = 25;
+
+        // Menus ---------------------------------------------------
+        validation = true;
+        goUp = false;
+        isPressed=false;
+        currentTime=0;
+        duration=1.1f;
+        distance=256f;
         startPos = board.transform.position;
         endPos = board.transform.position + Vector3.down * distance;
+        startPos2 = endPos;
+        endPos2 = startPos;
     }
 
     public void pressed(){
-        isPressed = true;
-        //navigation.SetActive(false);
-        //navigation_optional.SetActive(true);
+        if(validation == true){
+            isPressed = true;
+            currentTime=0;
+            validation = false;
+            //navigation.SetActive(false);
+            //navigation_optional.SetActive(true);
+        }
+    }
+
+    public void close(){
+        if(validation == true){
+            validation = false;
+            isPressed = true;
+            goUp = true;
+            currentTime=0;
+            startPos2 = board.transform.position;
+            endPos2 = board.transform.position + Vector3.up * distance;
+            //navigation.SetActive(false);
+            //navigation_optional.SetActive(true);
+        }
     }
 
     void Update(){
-        if(isPressed == true) {
+        //Resources
+        GameObject.FindWithTag("stone_counter").GetComponent<Text>().text = stone.ToString();
+        GameObject.FindWithTag("gold_counter").GetComponent<Text>().text = gold.ToString(); 
+        GameObject.FindWithTag("silver_counter").GetComponent<Text>().text = silver.ToString(); 
+
+        // Menus ------------------------------------------------------
+        if(isPressed == true && goUp == false && validation == false) {
             currentTime += Time.deltaTime;
             if(currentTime >= duration){
                 isPressed = false;
+                goUp = true;
+                validation = true;
                 //navigation.SetActive(true);
                 //navigation_optional.SetActive(false);
             }
 
             float Perc = currentTime/duration;
             board.transform.position = Vector3.Lerp(startPos,endPos,Perc);
-       }
+        }
+        if(isPressed == true && goUp == true && validation == false) {
+            currentTime += Time.deltaTime;
+            if(currentTime >= duration){
+                isPressed = false;
+                goUp = false;
+                validation = true;
+                //navigation.SetActive(true);
+                //navigation_optional.SetActive(false);
+            }
+
+            float Perc = currentTime/duration;
+            board.transform.position = Vector3.Lerp(startPos2,endPos2,Perc);
+        }
     }
 }
