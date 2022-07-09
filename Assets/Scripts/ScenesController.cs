427351d02f1c5e3f039e1089790dc0f396f5cbc5
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-07-09 21:44:47 +0100

    +

--- Assets/Scripts/ScenesController.cs
+++ Assets/Scripts/ScenesController.cs
@@ -14,50 +14,41 @@ public class ScenesController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        menuLoading = Resources.Load<Sprite>("Images/Loading_Initial");
-        cityLoading = Resources.Load<Sprite>("Images/Loading_city");
-        gunFightLoading = Resources.Load<Sprite>("Images/Loading_gun_fight");
-        mineLoading = Resources.Load<Sprite>("Images/Loading_mine");
+        menuLoading = Resources.Load<Sprite>("Loading_Initial");
+        cityLoading = Resources.Load<Sprite>("Loading_city");
+        gunFightLoading = Resources.Load<Sprite>("Loading_gun_fight");
+        mineLoading = Resources.Load<Sprite>("Loading_mine");
     }
 
     public void loudMenu(){
         //save
 
-        loading.SetActive(true);
         loading.GetComponent<UnityEngine.UI.Image>().sprite = menuLoading; 
-        StartCoroutine(ShowAndHide(loading, 1.0f)); // 1 second
-        loading.SetActive(false);
+        StartCoroutine(ShowAndHide(loading, 2.0f)); // 2 second
         SceneManager.LoadScene(0);
     }
 
     public void loudCity(){
         //save
 
-        loading.SetActive(true);
         loading.GetComponent<UnityEngine.UI.Image>().sprite = cityLoading;
-        StartCoroutine(ShowAndHide(loading, 1.0f)); // 1 second
-
-        loading.SetActive(false);
+        StartCoroutine(ShowAndHide(loading, 2.0f)); // 2 second
         SceneManager.LoadScene(1);
     }
 
     public void loudMine(){
         //save
 
-        loading.SetActive(true);
         loading.GetComponent<UnityEngine.UI.Image>().sprite = mineLoading;
-        StartCoroutine(ShowAndHide(loading, 1.0f)); // 1 second
-        loading.SetActive(false);
+        StartCoroutine(ShowAndHide(loading, 2.0f)); // 2 second
         SceneManager.LoadScene(2);
     }
 
     public void loadGunFight(){
         //save
 
-        loading.SetActive(true);
         loading.GetComponent<UnityEngine.UI.Image>().sprite = gunFightLoading;
-        StartCoroutine(ShowAndHide(loading, 1.0f)); // 1 second.
-        loading.SetActive(false);
+        StartCoroutine(ShowAndHide(loading, 2.0f)); // 2 second.
         SceneManager.LoadScene(3);
     }
 
