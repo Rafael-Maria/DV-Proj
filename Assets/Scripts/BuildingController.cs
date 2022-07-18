b40d8ddc0b501907b1d8318d28dc559518003814
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-07-18 01:12:29 +0100

    +

--- Assets/Scripts/BuildingController.cs
+++ Assets/Scripts/BuildingController.cs
@@ -17,6 +17,11 @@ public class BuildingController : MonoBehaviour
 
     [SerializeField] private GameObject levelTxt;
 
+    [SerializeField] private GameObject menuText;
+    [SerializeField] private GameObject upgradeText;
+    [SerializeField] private GameObject upgradeButton;
+
+
     void Start()
     {
         gameController = GameObject.FindGameObjectWithTag("gameController").GetComponent<GameController>();
@@ -76,6 +81,7 @@ public class BuildingController : MonoBehaviour
                 }
             }
             changeLevel(GetLevel());
+            changeText();
         }
     }
 
@@ -152,6 +158,7 @@ public class BuildingController : MonoBehaviour
                             gameController.RemoveStone(2900);
                             gameController.RemoveSilver(1500);
                             gameController.RemoveGold(800);
+                            upgradeButton.SetActive(false);
                             return true;
                         }
                         break;
@@ -167,6 +174,7 @@ public class BuildingController : MonoBehaviour
                             gameController.RemoveStone(2600);
                             gameController.RemoveSilver(1150);
                             gameController.RemoveGold(700);
+                            upgradeButton.SetActive(false);
                             return true;
                         }
                         break;
@@ -189,6 +197,7 @@ public class BuildingController : MonoBehaviour
                             gameController.RemoveStone(9000);
                             gameController.RemoveSilver(4100);
                             gameController.RemoveGold(1600);
+                            upgradeButton.SetActive(false);
                             return true;
                         }
                         break;
@@ -226,6 +235,7 @@ public class BuildingController : MonoBehaviour
                             gameController.RemoveStone(7000);
                             gameController.RemoveSilver(3800);
                             gameController.RemoveGold(1400);
+                            upgradeButton.SetActive(false);
                             return true;
                         }
                         break;
@@ -267,6 +277,7 @@ public class BuildingController : MonoBehaviour
                             gameController.RemoveSilver(2800);
                             gameController.RemoveGold(1100);
                             gameController.AddCitizens(80);
+                            upgradeButton.SetActive(false);
                             return true;
                         }
                         break;
@@ -278,6 +289,95 @@ public class BuildingController : MonoBehaviour
         }
         //Debug.Log("NICEEE");
         return false;
+    }
+
+    public void changeText(){
+        switch (name.ToString()){
+            case "Warehouse":
+                switch (GetLevel()){
+                    case 0:
+                        menuText.GetComponent<UnityEngine.UI.Text>().text = "The higher the level of the Warehouse, the more resources you can have.\nStorage: \n100           0           0 ";
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n90             0            0  \nStorage improvement:\nStone: 100 + 500\nSilver: 0 + 350\nGold: 0";
+                        break;
+                    case 1:
+                        menuText.GetComponent<UnityEngine.UI.Text>().text = "The higher the level of the Warehouse, the more resources you can have.\nStorage: \n600       350           0 ";
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n550         300            0    \nStorage improvement:\nStone: 600 + 1000\nSilver: 350 + 450\nGold: 0";
+                        break;
+                    case 2:
+                        menuText.GetComponent<UnityEngine.UI.Text>().text = "The higher the level of the Warehouse, the more resources you can have.\nStorage: \n1600       900           0   ";
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n1500         800            0     \nStorage improvement:\nStone:1600 + 3400\nSilver: 900 + 700\nGold: 0 + 900";
+                        break;
+                    case 3:
+                        menuText.GetComponent<UnityEngine.UI.Text>().text = "The higher the level of the Warehouse, the more resources you can have.\nStorage: \n3000     1600       900   ";
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n2900       1500        800     \nStorage improvement:\nStone: 3000 + 7000\nGold: 50 + 70\nSilver: 30 + 30";
+                        break;
+                    case 4:
+                        menuText.GetComponent<UnityEngine.UI.Text>().text = "The higher the level of the Warehouse, the more resources you can have.\nStorage: \n10000     5000     2000     ";
+                        break;
+                }
+                break;
+            case "Sheriff":
+                switch (GetLevel()){
+                    case 0:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n2600       1150        700     \n\nAn assault chance is reduced by 70%.";
+                        break;
+                    case 1:
+                        //ja ta no ultimo nivel (meter aviso)
+                        break;
+                }
+                break;
+            case "Saloon":
+                switch (GetLevel()){
+                    case 0:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n550         300             0  \n\nWorker performance is improved by 10%.";
+                        break;
+                    case 1:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n9000       4100       1600    \n\nWorker performance is improved by 10%.";
+                        break;
+                    case 2:
+                        //ja ta no ultimo nivel (meter aviso)
+                        break;
+                }
+                break;
+            case "Stable":
+                switch (GetLevel()){
+                    case 0:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n400         250             0  \n\nThe time it takes for resources to reach the city will be reduced by 10%.";
+                        break;
+                    case 1:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n1300         750             0    \n\nThe time it takes for resources to reach the city will be reduced by 10%.";
+                        break;
+                    case 2:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n2300       1200         650    \n\nThe time it takes for resources to reach the city will be reduced by 10%.";
+                        break;
+                    case 3:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n7000       3800       1400    \n\nThe time it takes for resources to reach the city will be reduced by 10%.";
+                        break;
+                    case 4:
+                        //ja ta no ultimo nivel (meter aviso)
+                        break;
+                }
+                break;
+            case "Home":
+                switch (GetLevel()){
+                    case 0:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n200           90             0  \nThe city now has twice as many workers.\nCitizens: 10 + 10";
+                        break;
+                    case 1:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n800         400             0  \nThe city now has twice as many workers.\nCitizens: 20 + 20";
+                        break;
+                    case 2:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n1800         800         400    \nThe city now has twice as many workers.\nCitizens: 40 + 40";
+                        break;
+                    case 3:
+                        upgradeText.GetComponent<UnityEngine.UI.Text>().text = "Cost:\n5000       2800       1100    \nThe city now has twice as many workers.\nCitizens: 80 + 80";
+                        break;
+                    case 4:
+                        //ja ta no ultimo nivel (meter aviso)
+                        break;
+                }
+                break;
+        }
     }
 
     IEnumerator ShowAndHide(GameObject go, float delay){
